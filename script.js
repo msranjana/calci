69f99f65d6b9475e132f1952298650f331533987
Ranjana <155282747+ranjanaaa04@users.noreply.github.com>
2024-09-01 08:30:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const display = document.querySelector(".display");
+  const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
 const specialChars = ["%", "*", "/", "-", "+", "="];
 let output = "";
@@ -6,12 +6,15 @@ let output = "";
 const calculate = (btnValue) => {
   display.focus();
   if (btnValue === "=" && output !== "") {
+   
     output = eval(output.replace("%", "/100"));
   } else if (btnValue === "reset") {
     output = "";
   } else if (btnValue === "clear") {
+   
     output = output.toString().slice(0, -1);
   } else {
+    
     if (output === "" && specialChars.includes(btnValue)) return;
     output += btnValue;
   }
@@ -19,5 +22,7 @@ const calculate = (btnValue) => {
 };
 
 buttons.forEach((button) => {
+ 
   button.addEventListener("click", (e) => calculate(e.target.dataset.value));
 });
+     
